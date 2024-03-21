82207e1ff58f16097ec1c4aa502633974ab36f07
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-21 04:19:05 +0000

    8

---
+++ lakher/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+
+    return (
+        <main className="bg-black">
+            <Navbar />
+        </main>
+    )
+}
+
+export default App
